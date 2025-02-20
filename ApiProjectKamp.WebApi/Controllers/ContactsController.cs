8dc17a9e0cd988061b91858421cc0c07669a1a6a
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-02-20 18:53:56 +0300

    Api Proje Kampı
    #9 - Dto ile Manuel Mapleme İşlemi
    #10 - Contact Apinin Test Edilmesi
    #11 - General Mapping ve Auto Mapper

---
+++ ApiProjectKamp.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,74 @@
+﻿using ApiProjectKamp.WebApi.Context;
+using ApiProjectKamp.WebApi.Dtos.ContactDtos;
+using ApiProjectKamp.WebApi.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactsController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            Contact contact = new Contact();
+            contact.Address = createContactDto.Address;
+            contact.Email = createContactDto.Email;
+            contact.MapLocation = createContactDto.MapLocation;
+            contact.OpenHours = createContactDto.OpenHours;
+            contact.Phone = createContactDto.Phone;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("İletişim bilgisi sisteme başarıyla eklendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _context.Contacts.Find(id);
+            _context.Contacts.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = _context.Contacts.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            Contact contact = new Contact();
+            contact.ContactId = updateContactDto.ContactId;
+            contact.Address = updateContactDto.Address;
+            contact.Email = updateContactDto.Email;
+            contact.OpenHours = updateContactDto.OpenHours;
+            contact.MapLocation = updateContactDto.MapLocation;
+            contact.Phone = updateContactDto.Phone;
+            _context.Contacts.Update(contact);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
---
+++ ApiProjectKamp.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjectKamp.WebApi.Dtos.FeaturesDtos;
+using ApiProjectKamp.WebApi.Dtos.MessageDtos;
+using ApiProjectKamp.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjectKamp.WebApi.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetFeatureByIdDto>().ReverseMap();
+
+            CreateMap<Message, ResultFeatureDto>().ReverseMap();
+            CreateMap<Message, CreateFeatureDto>().ReverseMap();
+            CreateMap<Message, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Message, GetMessageByIdDto>().ReverseMap();
+        }
+    }
+}
