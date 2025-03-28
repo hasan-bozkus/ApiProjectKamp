3f5b0a77d2183e25d79ed65c4ac330226910e21b
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-28 22:44:59 +0300

    Api Proje Kampı
    #19 - UI Projesinin Oluşturulması
    #20 - View Component Kullanımı

---
+++ ApiProjeKamp.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiProjeKamp.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
