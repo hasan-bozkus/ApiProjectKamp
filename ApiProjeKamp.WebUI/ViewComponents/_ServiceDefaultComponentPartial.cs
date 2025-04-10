f834b5e756d98a154674ff5149a559d120cbd1c5
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-04-10 10:50:20 +0300

    Api Proje Kampı
    #21 -  View Componentlerin Devamı
    #22 - ChatGpt ile Örnek Veri Seti Oluşturma
    #23 - Api Consume Backend Kodları
    #24 - Api Consume ile Servis Verilerinin Listelenmesi

---
+++ ApiProjeKamp.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjeKamp.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKamp.WebUI.ViewComponents
+{
+    public class _ServiceDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:4054/api/Services");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProjectKamp.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,58 @@
+﻿using ApiProjectKamp.WebApi.Context;
+using ApiProjectKamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ServicesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _context.Services.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return Ok("Servis ekleme işlemi başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteService(int id)
+        {
+            var result = _context.Services.Find(id);
+            _context.Services.Remove(result);
+            _context.SaveChanges();
+            return Ok("Servis silme işlemi başarılı.");
+        }
+
+        [HttpGet("GetService")]
+        public IActionResult GetService(int id)
+        {
+            var values = _context.Services.Find(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+            return Ok("Servis güncelleme işlemi başarılı.");
+        }
+    }
+}
