03668e84dbff05e70ae874f367b9c2a7ef20df52
Hasan BOZKUŞ <hasanbozkus0147@outlook.com>
2025-05-13 19:30:40 +0300

    Api Proje Kampı
    Bölüm 28 - Events Api

---
+++ ApiProjectKamp.WebApi/Controllers/YummyEventsController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProjectKamp.WebApi.Context;
+using ApiProjectKamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjectKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YummyEventsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public YummyEventsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult YummyEventList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Add(yummyEvent);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteYummyEvent(int id)
+        {
+            var result = _context.YummyEvents.Find(id);
+            _context.YummyEvents.Remove(result);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı.");
+        }
+
+        [HttpGet("GetYummyEvent")]
+        public IActionResult GetYummyEvent(int id)
+        {
+            var values = _context.YummyEvents.Find(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateYummyEvent(YummyEvent yummyEvent)
+        {
+            _context.YummyEvents.Update(yummyEvent);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı.");
+        }
+    }
+}
