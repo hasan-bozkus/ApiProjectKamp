c25bb02c313dd2b4276c9ede064092d127e33f11
Hasan BOZKUŞ <hasanbozkus0147@outlook.com>
2025-06-08 15:09:44 +0300

    Api Proje Kampı
    Bölüm 35: Navbarda Notification Listesi
    Bölüm 36: Navbar İşlemlerinin Tamamlanması

---
+++ ApiProjeKamp.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarAccountMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarAccountMenuAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
