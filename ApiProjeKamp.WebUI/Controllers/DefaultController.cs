ffdf4b8e0d5a4d629ed5c49e232a1c2fbd59cccb
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-08-27 14:07:54 +0300

    Api Proje Kampı
    Bölüm 60: Ana Sayfada Mesaj Gönderme İşlemi
    Bölüm 61: Hugging Face ve Restoranlar İçin Akıllı Yapay Zeka Öneri Sistemleri
    Bölüm 62: Hugging Face Helsinki Dil Modeli ile Yapay Zeka Üzerinden Çeviri İşlemi
    Bölüm 63: Hugging Face AI ile Toksik Mesaj Tespiti
    Bölüm 64: Yapay Zeka ile Toksik Mesaj Tespiti
    Bölüm 65: SignalR ile Anlık OpenAI ile Chatleşme / 1
    Bölüm 66: SignalR ile Anlık OpenAI ile Chatleşme / 2
    Bölüm 67: Delta, Choice ve Chat Stream Chunk Sınıfları

--- ApiProjeKamp.WebUI/Controllers/DefaultController.cs
+++ ApiProjeKamp.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,106 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKamp.WebUI.Dtos.MessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ApiProjeKamp.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+        {
+
+            var client = new HttpClient();
+            var apiKey = "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";
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+            try
+            {
+                var translateRequestBody = new
+                {
+                    inputs = createMessageDto.MessageDetails,
+                };
+                var translateJson = JsonConvert.SerializeObject(translateRequestBody);
+                var translateContent = new StringContent(translateJson, Encoding.UTF8, "application/json");
+
+                var translateResponse = await client.PostAsync("https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-tr-en", translateContent);
+                var translateResponseString = await translateResponse.Content.ReadAsStringAsync();
+
+                string englisText = createMessageDto.MessageDetails;
+                if (translateResponseString.TrimStart().StartsWith("["))
+                {
+                    var translateDoc = JsonDocument.Parse(translateResponseString);
+                    englisText = translateDoc.RootElement[0].GetProperty("translation_text").GetString();
+                    ViewBag.EnglisText = englisText;
+                }
+
+                var toxicRequestBody = new
+                {
+                    inputs = englisText
+                };
+
+                var toxicJson = JsonConvert.SerializeObject(toxicRequestBody);
+                var toxicContent = new StringContent(toxicJson, Encoding.UTF8, "application/json");
+                var toxicResponse = await client.PostAsync("https://api-inference.huggingface.co/models/unitary/toxic-bert", toxicContent);
+                var toxicResponseString = await toxicResponse.Content.ReadAsStringAsync();
+
+                if (toxicResponseString.TrimStart().StartsWith("["))
+                {
+                    var toxicDoc = JsonDocument.Parse(toxicResponseString);
+                    foreach(var item in toxicDoc.RootElement[0].EnumerateArray())
+                    {
+                        string label = item.GetProperty("label").GetString();
+                        double score = item.GetProperty("score").GetDouble();
+
+                        if(score > 0.5)
+                        {
+                            createMessageDto.Status = "Toksik Mesaj";
+                        }
+                    }
+                }
+                if(string.IsNullOrEmpty(createMessageDto.Status))
+                {
+                    createMessageDto.Status = "Mesaj Alındı";
+                }
+            }
+            catch (Exception ex)
+            {
+                createMessageDto.Status = "Onay Bekliyor";
+            }
+
+            createMessageDto.SendDate = DateTime.Now;
+            createMessageDto.IsRead = false;
+
+            var client2 = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMessageDto);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMesasge = await client2.PostAsync("https://localhost:44349/api/Messages", stringContent);
+            if (responseMesasge.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
