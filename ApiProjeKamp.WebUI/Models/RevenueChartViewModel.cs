c4dc948a4870e69160e6855f76ae364f2b766954
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-09-21 20:35:34 +0300

    Api Proje Kampı
    Bölüm 72: ChatGpt ile Rezervasyon Datası Oluşturma
    Bölüm 73: Son 4 Aylık Rezervasyonları Api ile Gruplama
    Bölüm 74: Dashboardda Ana Grafiğin Tamamlanması

--- ApiProjeKamp.WebUI/Models/RevenueChartViewModel.cs
+++ ApiProjeKamp.WebUI/Models/RevenueChartViewModel.cs
@@ -6,15 +6,20 @@
         public List<int> Income { get; set; } = new();
         public List<int> Expense { get; set; } = new();
 
-        // Alt kutucuk verileri
-        public decimal WeeklyEarnings { get; set; }
-        public decimal MonthlyEarnings { get; set; }
-        public decimal YearlyEarnings { get; set; }
+        //rezervasonlardan hesaplanacak ek veriler
+        public int TotalReservations { get; set; }
+        public int ActiveReservations { get; set; }
+        public int CancelledReservations { get; set; }
 
-        public int TotalCustomers { get; set; }
-        public decimal TotalIncome { get; set; }
-        public int ProjectCompleted { get; set; }
-        public decimal TotalExpense { get; set; }
-        public int NewCustomers { get; set; }
+        // Alt kutucuk verileri
+        //public decimal WeeklyEarnings { get; set; }
+        //public decimal MonthlyEarnings { get; set; }
+        //public decimal YearlyEarnings { get; set; }
+
+        //public int TotalCustomers { get; set; }
+        //public decimal TotalIncome { get; set; }
+        //public int ProjectCompleted { get; set; }
+        //public decimal TotalExpense { get; set; }
+        //public int NewCustomers { get; set; }
     }
 }
